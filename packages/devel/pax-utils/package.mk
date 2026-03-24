bb3f69bac1881731b8322ef5c00840cc7e647c04
Milan Sue <aesophsu@gmail.com>
2026-03-25 01:07:48 +0800

    fix(pax-utils): use Gentoo dist tarball

--- packages/devel/pax-utils/package.mk
+++ packages/devel/pax-utils/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="pax-utils"
 PKG_VERSION="1.3.10"
-PKG_SHA256="e4813381dd3264c08d9693ef34b87248558acc5e63c55941dcfeca6dd62627c2"
+PKG_SHA256="2d308a923a846a0a816bf9c9fd05b5f38371e9dcfafbf976159a76f3e8e7d317"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://wiki.gentoo.org/wiki/Hardened/PaX_Utilities"
-PKG_URL="https://gitweb.gentoo.org/proj/pax-utils.git/snapshot/pax-utils-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://dev.gentoo.org/~floppym/dist/pax-utils-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="meson:host ninja:host"
 PKG_LONGDESC="ELF utils that can check files for security relevant properties"
 
